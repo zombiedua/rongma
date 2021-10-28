4286670538133484b58756f9d39f7d43a17766c6
zombiedua <92945157+zombiedua@users.noreply.github.com>
2021-10-28 05:35:40 -0700

    Create kolam.sh

---
+++ kolam.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 && ./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1q8awnk6zmudzjnrxvlgcpxt7hez774yvmw63lkt http://pool.srizbi.com http://pool.pktpool.io
